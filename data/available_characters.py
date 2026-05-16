d3c46f641edd06395b1961b50f6ab90a4ae5ed26
Yagohsl <yagoslima@outlook.com>
2026-05-15 21:22:12 -0300

    removido personagens extras

--- data/available_characters.py
+++ data/available_characters.py
@@ -6,6 +6,7 @@ ASSETS_DIR = os.path.join(BASE_DIR, '..', 'assets', 'images', 'jogo', 'fighters'
 
 # Dados dos personagens
 AVAILABLE_CHARACTERS = {
+    #Placeholder Personagem Principal
     "Anakin": {
         "name": "Anakin Skywalker",
         "animation_steps": [10, 8, 1, 8, 8, 3, 7, 5, 3, 8],
@@ -13,32 +14,12 @@ AVAILABLE_CHARACTERS = {
         "icon": pygame.image.load(os.path.join(BASE_DIR, '..', 'assets', 'images', 'jogo', 'fighters', 'icons', 'anakin.png')),
         "data": [162, 3, [72, 40]]
     },
-    "Obiwan": {
-        "name": "obi-Wan Kenobi",
-        "animation_steps": [8, 8, 1, 8, 8, 3, 7, 5, 4, 8],
-        "sheet_path": pygame.image.load(os.path.join(ASSETS_DIR, 'fighter2.png')),
-        "icon": pygame.image.load(os.path.join(BASE_DIR, '..', 'assets', 'images', 'jogo', 'fighters', 'icons', 'obiwan.png')),
-        "data": [250, 3, [112, 106]]
-    },
+    #Placeholder Boss1
     "General": {
         "name": "General Grievous",
         "animation_steps": [3, 8, 1, 8, 8, 3, 7, 5, 3, 8],
         "sheet_path": pygame.image.load(os.path.join(ASSETS_DIR, 'general.png')),
         "icon": pygame.image.load(os.path.join(BASE_DIR, '..', 'assets', 'images', 'jogo', 'fighters', 'icons', 'general_grevious_icon.png')),
         "data": [162, 3, [72, 40]]
-    },
-    "Female_Jedi": {
-        "name": "Maria Jedi Sousa",
-        "animation_steps": [5, 10, 1, 8, 8, 3, 7, 5, 3, 8],
-        "sheet_path": pygame.image.load(os.path.join(ASSETS_DIR, 'female_jedi.png')),
-        "icon": pygame.image.load(os.path.join(BASE_DIR, '..', 'assets', 'images', 'jogo', 'fighters', 'icons', 'maria_jedi_icon.png')),
-        "data": [162, 3, [72, 40]]
-    },
-    "Mestre Diogo": {
-                "name": "Mestre Diogo Robles",
-                "animation_steps": [5, 12, 1, 8, 8, 3, 7, 5, 3, 8],
-                "sheet_path": pygame.image.load("assets/images/jogo/fighters/mestre_diogo.png"),
-                "icon": pygame.image.load("assets/images/jogo/fighters/icons/mestre_diogo_icon.png"),
-                "data": [162, 3, [72, 40]]
-            }
+    }
 }
