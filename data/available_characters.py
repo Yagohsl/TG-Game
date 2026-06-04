40af574695f584c5e8f2cdb1a5d942aae363a912
Yagohsl <yagoslima@outlook.com>
2026-06-04 17:51:52 -0300

    add sprite pular

--- data/available_characters.py
+++ data/available_characters.py
@@ -10,17 +10,17 @@ AVAILABLE_CHARACTERS = {
     #Placeholder Personagem Principal
     "Mestre Diogo": {
                 "name": "Mestre Diogo Robles",
-                "animation_steps": [1, 12, 1, 8, 8, 3, 7, 5, 3, 8],
-                "sheet_path": pygame.image.load("assets/images/jogo/fighters/mestre_diogo.png"),
+                "animation_steps": [1, 5, 1, 1, 1, 1, 1, 1, 1, 1],
+                "sheet_path": pygame.image.load("assets/images/jogo/fighters/hero.png"),
                 "icon": pygame.image.load("assets/images/jogo/fighters/icons/mestre_diogo_icon.png"),
-                "data": [162, 3, [72, 40]]
+                "data": [64, 3, [22, 13]]
             },
-    "General": {
-        "name": "General Grievous",
-        "animation_steps": [3, 8, 1, 8, 8, 3, 7, 5, 3, 8],
-        "sheet_path": pygame.image.load(os.path.join(ASSETS_DIR, 'general.png')),
-        "icon": pygame.image.load(os.path.join(BASE_DIR, '..', 'assets', 'images', 'jogo', 'fighters', 'icons', 'general_grevious_icon.png')),
-        "data": [162, 3, [72, 40]]
+    "Hero": {
+        "name": "Herói",
+        "animation_steps": [1, 5, 2, 1, 1, 1, 1, 1, 1, 1],
+        "sheet_path": pygame.image.load(os.path.join(ASSETS_DIR, 'hero.png')),
+        "icon": pygame.image.load(os.path.join(BASE_DIR, '..', 'assets', 'images', 'jogo', 'fighters', 'icons', 'mestre_diogo_icon.png')),
+        "data": [64, 3, [22, 13]]
     },
     "Anxiety": {
         "name": "Ansiedade",
